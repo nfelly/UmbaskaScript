2e1499457ca5173cfdfb268256fc7e62e1c87a9c
nfelly <nfell2009@outlook.com>
2015-07-07 17:02:38 +0100

    50% working

--- src/uk/co/umbaska/umbaskascript/ScriptLoader.php
+++ src/uk/co/umbaska/umbaskascript/ScriptLoader.php
@@ -2,22 +2,27 @@
 namespace uk\co\umbaska\umbaskascript;
 
 use pocketmine\Server;
-use pocketmine\plugin\PluginBase as Base;
 
-use uk\co\umbaska\umbaskascript\UmbaskaScript as Script;
+use uk\co\umbaska\umbaskascript\UmbaskaScript;
 
 class ScriptLoader {
+
+	public function __construct(UmbaskaScript $plugin) {
+        $this->plugin = $plugin;
+    }
+
     public static function loadScripts() {
-		$scripts = scandir("/plugins/UmbaskaScript/scripts/");
+		$scripts = scandir($this->plugin->getDataFolder() . "scripts/");
+		$counter = 0;
 		foreach ($scripts as &$file) {
 			$contents = file($file);
-			$counter = 0;
 			foreach($contents as $line) {
 				if ($line == "on block break:") {
 					++$counter;
-					array_push(Script::returnEventsArray(), "on block break::" . $file . "::" . $counter);
+					array_push(UmbaskaScript::returnEventsArray(), "on block break::" . $file . "::" . $counter);
 				}
 			}
 		}
+		$this->plugin->getServer()->getLogger()->error("Loaded: " . $counter . " scripts!");
 	}
 }
--- src/uk/co/umbaska/umbaskascript/UmbaskaScript.php
+++ src/uk/co/umbaska/umbaskascript/UmbaskaScript.php
@@ -4,21 +4,22 @@ namespace uk\co\umbaska\umbaskascript;
 use pocketmine\Server;
 use pocketmine\plugin\PluginBase as Base;
 
-//use uk\co\umbaska\umbaskascript\ScriptLoader as Loader;
+use uk\co\umbaska\umbaskascript\ScriptLoader;
 
 class UmbaskaScript extends Base {
     public function onEnable() {
-        if (!file_exists("/plugins/UmbaskaScript/")) {
-    	    mkdir("/plugins/UmbaskaScript/");
-    	}
-		if (!file_exists("/plugins/UmbaskaScript/scripts/")) {
-    	    mkdir("/plugins/UmbaskaScript/scripts/");
-    	}
-		//Loader::loadScripts();
+        if (!is_dir($this->getDataFolder())) mkdir($this->getDataFolder());
+		if (!is_dir($this->getDataFolder() . "scripts/")) mkdir($this->getDataFolder() . "scripts/");
+		ScriptLoader::loadScripts();
 	}
-	/*public static $eventsArray = array();
+	
+	public function returnThis() {
+		return $this;
+	}
+	
+	public static $eventsArray = array();
 	
 	public function returnEventsArray() {
 		return self::$eventsArray();
-	}*/
+	}
 }
